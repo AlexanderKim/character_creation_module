c641353dc7c0005763f4543acf47e6842a404fe4
Alexander Kim <alkim.mailbox@gmail.com>
2023-10-06 18:21:38 +0200

    Added docstring inline doc

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack and do damage according to the character class."""
+
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 '{5 + randint(3, 5)}')
@@ -15,6 +19,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defend from a damage based on character class."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал'
                 '{10 + randint(5, 10)} урона')
@@ -28,6 +33,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Cast a special skill."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 '«Выносливость {80 + 25}»')
@@ -40,6 +46,8 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Start training loop."""
+
     if char_class == 'warrior':
         print(f'{char_name}, '
               'ты Воитель — отличный боец ближнего боя.')
@@ -67,6 +75,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Select character class."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -88,7 +97,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -98,6 +108,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
